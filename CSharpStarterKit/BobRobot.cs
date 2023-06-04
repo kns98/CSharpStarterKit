9ec82662b5cc81ab764998c5d8305d0374e9eebc
kns98 <kns98@yahoo.com>
2023-06-04 15:11:31 +0800

    bob

---
+++ CSharpStarterKit/BobRobot.cs
@@ -0,0 +1,90 @@
+﻿using CSharpLearn;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn
+{
+    internal class BobRobot
+    {
+        public static void Bob(Language languageChoice)
+        {
+            // English: Implementation for the Bob problem
+            // Chinese: Bob问题的实现
+            string[] languageStrings = languageChoice == Language.English ?
+                new string[] { "Enter a phrase to Bob: ", "Bob's response: " } :
+                new string[] { "输入要与Bob交流的短语: ", "Bob的回应: " };
+
+            Console.Write(languageStrings[0]);
+            string phrase = Console.ReadLine();
+            string response = "";
+            if (languageChoice == Language.English)
+            {
+                response = BobResponse(phrase);
+            }
+            else
+            {
+                response = BobResponse_Chinese(phrase);
+            }
+            Console.WriteLine(languageStrings[1] + response);
+        }
+
+        private static string BobResponse(string phrase)
+        {
+            if (string.IsNullOrWhiteSpace(phrase))
+            {
+                return "Fine. Be that way!";
+            }
+
+            bool isQuestion = phrase.EndsWith("?");
+            bool isYelling = phrase.ToUpper() == phrase;
+
+            if (isQuestion && isYelling)
+            {
+                return "Calm down, I know what I'm doing!";
+            }
+            else if (isQuestion)
+            {
+                return "Sure.";
+            }
+            else if (isYelling)
+            {
+                return "Whoa, chill out!";
+            }
+            else
+            {
+                return "Whatever.";
+            }
+        }
+
+        private static string BobResponse_Chinese(string phrase)
+        {
+            if (string.IsNullOrWhiteSpace(phrase))
+            {
+                return "好吧，随你的便！";
+            }
+
+            bool isQuestion = phrase.EndsWith("？");
+            bool isYelling = phrase.EndsWith("@");
+
+            if (isQuestion && isYelling)
+            {
+                return "冷静点，我知道我在做什么！";
+            }
+            else if (isQuestion)
+            {
+                return "当然。";
+            }
+            else if (isYelling)
+            {
+                return "哇，冷静！";
+            }
+            else
+            {
+                return "随你便吧。";
+            }
+        }
+    }
+}
--- CSharpStarterKit/Program.cs
+++ CSharpStarterKit/Program.cs
@@ -203,7 +203,7 @@ namespace CSharpLearn
                     Console.WriteLine("What is Bob?");
                     Console.WriteLine("Description: A conversational chatbot that responds to user input.");
                     Console.WriteLine("Usage: The Bob class utilizes natural language processing techniques to understand and interact with users.");
-                    Bob();
+                    BobRobot.Bob(languageChoice);
                     break;
                 case 8:
                     Console.WriteLine("Clock");
@@ -810,83 +810,7 @@ namespace CSharpLearn
         }
 
 
-        public static void Bob()
-        {
-            // English: Implementation for the Bob problem
-            // Chinese: Bob问题的实现
-            string[] languageStrings = languageChoice == Language.English ?
-                new string[] { "Enter a phrase to Bob: ", "Bob's response: " } :
-                new string[] { "输入要与Bob交流的短语: ", "Bob的回应: " };
 
-            Console.Write(languageStrings[0]);
-            string phrase = Console.ReadLine();
-            string response = "";
-            if (languageChoice == Language.English)
-            {
-                response = BobResponse(phrase);
-            }
-            else
-            {
-                response = BobResponse_Chinese(phrase);
-            }
-            Console.WriteLine(languageStrings[1] + response);
-        }
-
-        private static string BobResponse(string phrase)
-        {
-            if (string.IsNullOrWhiteSpace(phrase))
-            {
-                return "Fine. Be that way!";
-            }
-
-            bool isQuestion = phrase.EndsWith("?");
-            bool isYelling = phrase.ToUpper() == phrase;
-
-            if (isQuestion && isYelling)
-            {
-                return "Calm down, I know what I'm doing!";
-            }
-            else if (isQuestion)
-            {
-                return "Sure.";
-            }
-            else if (isYelling)
-            {
-                return "Whoa, chill out!";
-            }
-            else
-            {
-                return "Whatever.";
-            }
-        }
-
-        private static string BobResponse_Chinese(string phrase)
-        {
-            if (string.IsNullOrWhiteSpace(phrase))
-            {
-                return "好吧，随你的便！";
-            }
-
-            bool isQuestion = phrase.EndsWith("？");
-            bool isYelling = phrase.EndsWith("@");
-
-            if (isQuestion && isYelling)
-            {
-                return "冷静点，我知道我在做什么！";
-            }
-            else if (isQuestion)
-            {
-                return "当然。";
-            }
-            else if (isYelling)
-            {
-                return "哇，冷静！";
-            }
-            else
-            {
-                return "随你便吧。";
-            }
-        }
 
     }
 
