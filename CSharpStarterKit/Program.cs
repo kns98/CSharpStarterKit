e6ab64d40d5a36c5d7db9a1572d7f891a61d4e0f
kns98 <kns98@yahoo.com>
2023-06-04 09:59:56 +0800

    move to file

--- CSharpStarterKit/Program.cs
+++ CSharpStarterKit/Program.cs
@@ -247,7 +247,7 @@ namespace CSharpLearn
                     Console.WriteLine("Description: Counts the occurrences of nucleotides (A, C, G, T) in a DNA strand.");
                     Console.WriteLine("Input: A DNA strand as a string.");
                     Console.WriteLine("Output: Returns a dictionary or an associative array with the counts of each nucleotide.");
-                    NucleotideCount();
+                    NucleotideCountMod.NucleotideCount(languageChoice);
                     break;
                 case 14:
                     Console.WriteLine("Palindrome Products");
@@ -385,41 +385,6 @@ namespace CSharpLearn
 
             string binaryString = Convert.ToString(decimalNumber, 2);
             Console.WriteLine(languageStrings[1] + binaryString);
-        }
-
-        
-
-        public static void NucleotideCount()
-        {
-            // English: Implementation for the Nucleotide Count problem
-            // Chinese: 核苷酸计数问题的实现
-            string[] languageStrings = languageChoice == Language.English ?
-                new string[] { "Enter a DNA sequence: ", "A:", "C:", "G:", "T:" } :
-                new string[] { "输入一个DNA序列: ", "A:", "C:", "G:", "T:" };
-
-            Console.Write(languageStrings[0]);
-            string sequence = Console.ReadLine().ToUpper();
-
-            Dictionary<char, int> nucleotideCount = new Dictionary<char, int>
-        {
-            { 'A', 0 },
-            { 'C', 0 },
-            { 'G', 0 },
-            { 'T', 0 }
-        };
-
-            foreach (char nucleotide in sequence)
-            {
-                if (nucleotideCount.ContainsKey(nucleotide))
-                {
-                    nucleotideCount[nucleotide]++;
-                }
-            }
-
-            Console.WriteLine(languageStrings[1] + nucleotideCount['A']);
-            Console.WriteLine(languageStrings[2] + nucleotideCount['C']);
-            Console.WriteLine(languageStrings[3] + nucleotideCount['G']);
-            Console.WriteLine(languageStrings[4] + nucleotideCount['T']);
         }
 
         public static void PalindromeProducts()
