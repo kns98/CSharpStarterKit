33dbfb1b2a012d99697c49da8fa885888708c5bb
kns98 <kns98@yahoo.com>
2023-06-05 11:49:19 +0800

    beersong

--- CSharpStarterKit/Program.cs
+++ CSharpStarterKit/Program.cs
@@ -182,7 +182,7 @@ namespace CSharpLearn
                     Console.WriteLine("Description: Generates the lyrics of the classic '99 Bottles of Beer' song.");
                     Console.WriteLine("Input: The number of bottles to start with.");
                     Console.WriteLine("Output: Returns the lyrics of the song.");
-                    BeerSong();
+                    BeerSongMod.BeerSong(languageChoice);
                     break;
                 case 5:
                     Console.WriteLine("Binary");
@@ -314,32 +314,6 @@ namespace CSharpLearn
         {
             CSharpLearn.BinarySearchTree<object>.BCS(languageChoice);
         }
-
-        public static void BeerSong()
-        {
-            // English: Implementation for the Beer Song problem
-            // Chinese: 啤酒歌问题的实现
-            string bottles = languageChoice == Language.English ? " bottles" : " 瓶";
-            string bottle = languageChoice == Language.English ? " bottle" : " 瓶";
-            string onTheWall = languageChoice == Language.English ? " of beer on the wall," : " 的啤酒在墙上,";
-            string ofBeer = languageChoice == Language.English ? " of beer." : " 的啤酒.";
-            string takeOneDown = languageChoice == Language.English ? "Take one down and pass it around," : "拿掉一瓶传来传去,";
-            string noMoreBottles = languageChoice == Language.English ? "No more bottles of beer on the wall, no more bottles of beer." : "没有啤酒在墙上, 没有啤酒.";
-            string goToStore = languageChoice == Language.English ? " Go to the store and buy some more," : "去商店买一些,";
-            string bottlesOnTheWall = languageChoice == Language.English ? " bottles of beer on the wall." : " 瓶的啤酒在墙上.";
-
-            for (int i = 99; i > 0; i--)
-            {
-                Console.WriteLine($"{i}{(i == 1 ? bottle : bottles)}{onTheWall} {i}{(i == 1 ? bottle : bottles)}{ofBeer}");
-                Console.WriteLine(takeOneDown);
-                Console.WriteLine($"{i - 1}{((i - 1) == 1 ? bottle : bottles)}{goToStore}\n");
-            }
-
-            Console.WriteLine(noMoreBottles);
-            Console.WriteLine(goToStore.Trim());
-            Console.WriteLine($"99{bottles}{bottlesOnTheWall}\n");
-        }
-
 
         public static void Binary()
         {
