0ad01fdbb47f44b699ceb9025a6db8e4683d7f62
kns98 <kns98@yahoo.com>
2023-06-06 12:58:27 +0800

    triangle

--- CSharpStarterKit/Triangle.cs
+++ CSharpStarterKit/Triangle.cs
@@ -7,24 +7,60 @@ using System.Threading.Tasks;
 
 namespace CSharpStarterKit
 {
-    internal class TriangleMod
+    class TriangleMod
     {
-        public static void Triangle(Language languageChoice)
+        static string DetermineTriangleType(double side1, double side2, double side3)
         {
-            // English: Implementation for the Triangle problem
-            // Chinese: 三角形问题的实现
-            string[] languageStrings = languageChoice == Language.English ?
-                new string[] { "Enter the number of rows: " } :
-                new string[] { "输入行数: " };
-
-            Console.Write(languageStrings[0]);
-            int numRows = int.Parse(Console.ReadLine());
-
-            for (int i = 1; i <= numRows; i++)
+            if (side1 == side2 && side2 == side3)
             {
-                string row = new string('*', i);
-                Console.WriteLine(row);
+                return "equilateral";
+            }
+            else if (side1 == side2 || side1 == side3 || side2 == side3)
+            {
+                return "isosceles";
+            }
+            else
+            {
+                return "scalene";
+            }
+        }
+
+        internal static void Triangle(Language languageChoice)
+        {
+            if (languageChoice == Language.English)
+            {
+                Console.WriteLine("Enter the length of side 1:");
+                double side1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the length of side 2:");
+                double side2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the length of side 3:");
+                double side3 = double.Parse(Console.ReadLine());
+
+                string triangleType = DetermineTriangleType(side1, side2, side3);
+
+                Console.WriteLine("The triangle is " + triangleType);
+            }
+
+            if (languageChoice == Language.Chinese)
+            {
+                Console.WriteLine("输入边长1的长度：");
+                double side1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("输入边长2的长度：");
+                double side2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("输入边长3的长度：");
+                double side3 = double.Parse(Console.ReadLine());
+
+                string triangleType = DetermineTriangleType(side1, side2, side3);
+
+                Console.WriteLine("该三角形为" + triangleType + "三角形");
             }
         }
     }
+
 }
+
+
