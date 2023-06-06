eaeacc98f43ff6acf08c111b2009a443c9e6bb63
kns98 <kns98@yahoo.com>
2023-06-06 13:01:29 +0800

    trinary

--- CSharpStarterKit/Trinary.cs
+++ CSharpStarterKit/Trinary.cs
@@ -11,22 +11,24 @@ namespace CSharpStarterKit
     {
         public static void Trinary(Language languageChoice)
         {
-            // English: Implementation for the Trinary problem
-            // Chinese: 三进制问题的实现
+            // Define language-specific strings for user prompts and output
             string[] languageStrings = languageChoice == Language.English ?
                 new string[] { "Enter a trinary number: ", "The decimal representation is: " } :
                 new string[] { "输入一个三进制数: ", "十进制表示为: " };
 
+            // Prompt the user to enter a trinary number
             Console.Write(languageStrings[0]);
             string trinaryNumber = Console.ReadLine();
 
             int decimalNumber = 0;
             int power = 0;
 
+            // Convert the trinary number to decimal
             for (int i = trinaryNumber.Length - 1; i >= 0; i--)
             {
                 int digit = int.Parse(trinaryNumber[i].ToString());
 
+                // Check if the digit is valid (between 0 and 2)
                 if (digit < 0 || digit > 2)
                 {
                     Console.WriteLine("Invalid trinary number.");
@@ -37,7 +39,8 @@ namespace CSharpStarterKit
                 power++;
             }
 
+            // Output the decimal representation of the trinary number
             Console.WriteLine(languageStrings[1] + decimalNumber);
         }
+
     }
-}
